9963df471bd61b420369701e1590630692a5f882
David Costa <davidcostadev@gmail.com>
2019-10-30 13:16:50 -0300

    Add bulma and fontawesome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bulma';
+import '@fortawesome/fontawesome-free/js/all';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
